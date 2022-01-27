39aca364040c0b0be832b6e14d95192048cf2333
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-01-27 15:01:33 -0700

    L1E added

---
+++ Set1/L1E/main.cpp
@@ -0,0 +1,18 @@
+/* CSCI 261: Lab 1E: Makefiles 
+ *
+ * Author: Garrett Thompson
+ *
+ * XXXXXXXX (MORE_COMPLETE_DESCRIPTION_HERE)
+ */
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+
+// Define any constants below this comment.
+
+// Must have a function named "main", which is the starting point of a C++ program.
+int main() {
+  cout << "Hello World!" << endl;
+}
