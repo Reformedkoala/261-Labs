852f018109339dc926b22eb08ddbecccbca31015
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-01-28 12:37:55 -0700

    Completed Lab 1F

--- Set1/L1F/main.cpp
+++ Set1/L1F/main.cpp
@@ -1,6 +1,6 @@
 /* CSCI 261: Errors
  *
- * Author: XXXX (_INSERT_YOUR_NAME_HERE_)
+ * Author: Garrett Thompson
  *
  *    This program illustrates a variety of common errors.
  *    Fix the errors in each section.
@@ -38,7 +38,8 @@ int main() {
 
 // SECTION I: update comment below on how you fixed this section's code, and tests run
 // that show loop is working properly
-// FIX =
+// FIX = Fixing the syntax error of ci instead of cin and fixing the fact that it has to be less then or equal to because otherwise it won't
+// add the final value it needs to give the correct output.  Tested 1-5 and it gave 15 which is correct now.
 // TESTS:
 
     cout << endl;
@@ -50,23 +51,25 @@ int main() {
     int i;        // Used as loop control variable
     int numIter;  // The number of times to iterate
     cout << "Enter a number greater than 1 to sum up to: ";
-    ci >> numIter;
-    for (i = 1; i < numIter; ++i) {
+    cin >> numIter;
+    for (i = 1; i <= numIter; i++) {
          sum += i;
      }
     cout << "The sum of the numbers from 1 to " << numIter << " (inclusive) is: " << sum << endl;
 
 // SECTION II: update comment below on how you fixed this section's code, and tests run
 // that show loop is working properly
-// FIX =
+// FIX = Fixed syntax error on cot instead of cout.  Fixed stream direction error of the cout being >> instead of <<.  Fixed the fact that total
+// was being reset everytime in the while loop instead of being added to.  Tested with number of items being 5 and the price of them being
+// different each time to check if they add up correctly now.
 // TESTS:
 
     cout << endl;
     cout << "******************" << endl;
-    cot << "Section II" << endl;
+    cout << "Section II" << endl;
     cout << "******************" << endl;
 
-    double total;     // Accumulates total
+    double total = 0;     // Accumulates total
     double price;     // Gets next price from user
     int numItems;     // Number of items
     int counter = 1;  // Loop control counter
@@ -76,10 +79,9 @@ int main() {
     cout << endl;
 
     while (counter <= numItems) {
-        total = 0;
         cout << "Enter the price of item " << counter << ": ";
         cin >> price;
-        cout >> endl;
+        cout << endl;
         total += price;
         counter++;
     }
@@ -87,7 +89,11 @@ int main() {
 
 // SECTION III: update comment below on how you fixed this section's code, and tests run
 // that show loop is working properly
-// FIX =
+// FIX = No semicolon on what was line 105 so I added it.  Sum2 was never actually intialized as 0 so you can't add anything to it.  I added an
+// intialization to sum2.  Counter2 was never initialized nor was it being iterated as it increased each time in the loop.  Also the conditional
+// for the do-while loop was wrong and would result in it always stopping after 1 time.  Changed conditional to compare to numIter2 instead.
+// Just a formatting thing but instead of the user input being on a seperate line I changed it to be on the same line.  Tested the loop
+// with random values to make sure it was correctly outputting.
 // TESTS:
 
     cout << endl;
@@ -95,17 +101,18 @@ int main() {
     cout << "Section III" << endl;
     cout << "******************" << endl;
 
-    int sum2 ;
-    int counter2;
+    int sum2 = 0;
+    int counter2 = 1;
     int numIter2;
 
-    cout << "What number do you wish me to sum from 1 to?" << endl;
-    cin >> numIter2
+    cout << "What number do you wish me to sum from 1 to? ";
+    cin >> numIter2;
 
     do {
         sum2 += counter2;
         cout << "Sum so far: " << sum2 << endl;
-    } while (counter2 <= sum2);
+        counter2++;
+    } while (counter2 <= numIter2);
 
     cout << endl << "Section III Recap" << endl;
 
@@ -114,7 +121,9 @@ int main() {
 
 // SECTION IV: update comment below on how you fixed this section's code, and tests run
 // that show loop is working properly
-// FIX =
+// FIX = Mainly a logic error in the for loop causing it to add up all numbers greater then 0 essentially starting at where you enter though.
+// Fixed it so that it now iterates from 1 squared which is just 1 to the number you give it but squared like it should.  Fixed a logic error
+// that would cause it to not be inclusive as well.  Tested the loop with random values that gave the correct output.
 // TESTS:
 
     cout << endl;
@@ -123,21 +132,22 @@ int main() {
     cout << "******************" << endl;
 
     cout << "I will now calculate ";
-    cout << "the sum of 1 squared to ? squared (inclusive)" << endl;
-    
+    cout << "the sum of 1 squared to ? squared (inclusive): ";
     int numIter3;
     cin >> numIter3;
 
     int sum3 = 0;
-    for (int i3 = numIter3; i3 > 0; i3++) {
+    for (int i3 = 1; i3 <= numIter3*numIter3; i3++) {
         sum3 += i3*i3;
     }
 
     cout << "The sum of squares from 1 to " << numIter3 << " is: " << sum3 << endl;
 
 // SECTION V: update comment below on how you fixed this section's code, and tests run
-/ that show loop is working properly
-// FIX =
+// that show loop is working properly
+// FIX = Fixed comment syntax error.  Fixed the fact some variables were not initialized.  Fixed a logic error in the fact the loop would only 
+// iterate from 1 to 10.  Fixed another logic error in the it was incrementing the loop outside of itself.  Tested the loop with random values that
+// were correct.
 // TESTS:
 
     cout << endl;
@@ -146,16 +156,17 @@ int main() {
     cout << "******************" << endl;
 
     cout << "I will now calculate ";
-    cout << "the sum from 1 cubed to ? cubed (inclusive)" << endl;
+    cout << "the sum from 1 cubed to ? cubed (inclusive): ";
 
     int numIter4;
+    int counter4 = 1;
+    int sum4 = 0;
     cin >> numIter4;
 
-    while (counter4 < 10) {
+    while (counter4 <= numIter4) {
         sum4 += (counter4 * counter4 * counter4);
+        counter4++;
     }
-
-    counter4++;
 
     cout << "The sum of cubes from 1 to " << numIter4 << " is: " << sum4 << endl;
 
@@ -165,6 +176,5 @@ int main() {
     cout << "******************" << endl;
 
     cout << endl << "Congrats!  You fixed them all (hopefully correctly!)" << endl << endl << "Goodbye" << endl << endl;
-
     return 0;
 }
