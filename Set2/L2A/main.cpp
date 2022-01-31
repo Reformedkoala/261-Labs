f1490205216b4ba7106cd2274ecc47eb1cf21cf9
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-01-31 09:39:16 -0700

    Adding L2A

---
+++ Set2/L2A/main.cpp
@@ -0,0 +1,18 @@
+/* CSCI 261: Lab 2A: Secret Moosage 
+ *
+ * Author: Garrett Thompson
+ *
+ * 
+ */
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+
+// Define any constants below this comment.
+
+// Must have a function named "main", which is the starting point of a C++ program.
+int main() {
+  cout << "Hello World!" << endl;
+}
