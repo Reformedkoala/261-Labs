b66c277f7adbd16b68e1ea85cf7d258efa51f57e
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-02-07 16:20:04 -0700

    Finished L2B with extra credit

--- Set2/L2B/main.cpp
+++ Set2/L2B/main.cpp
@@ -14,39 +14,98 @@
 // We will (most of the time) use the standard library namespace in our programs.
 using namespace std;
 
-// Define any constants below this comment.
-void generate_random_integer(int min_val, int max_val, bool min_in, bool max_in) {
-  int i = 0;
-  for (i = 0; i < 10; i++) {
-    int rand_int = rand() % (int)((max_val - min_val + 1) + min_val);
-    cout << rand_int << endl;
-  }
-}
-void generate_random_float(float min_val, float max_val, bool min_in, bool max_in) {
-  int i = 0;
-  for (i =0; i < 10; i++) {
-    float rand_float = rand() / (float)RAND_MAX * (max_val - min_val) + min_val;
-    cout << setprecision(4) << rand_float << endl;
-  }
-}
+void generate_random_integer(int minVal, int maxVal, bool minIn, bool maxIn);
+void generate_random_float(float minVal, float maxVal, bool minIn, bool maxIn);
+
 // Must have a function named "main", which is the starting point of a C++ program.
 int main() {
   srand(time(0));
   rand();
-  float min_val;
-  float max_val;
-  bool min_in;
-  bool max_in;
+  float minVal;
+  float maxVal;
+  bool minIn;
+  bool maxIn;
   cout << "Enter the minimum value: ";
-  cin >> min_val;
+  cin >> minVal;
   cout << "Enter the maximum value: ";
-  cin >> max_val;
+  cin >> maxVal;
   cout << "Enter whether or not the minimum should be inclusive ";
-  cin >> min_in;
+  cin >> minIn;
   cout << "Enter whether or not the maximum should be inclusive ";
-  cin >> max_in;
-  generate_random_integer(min_val, max_val, min_in, max_in);
-  generate_random_float(min_val, max_val, min_in, max_in);
+  cin >> maxIn;
+  generate_random_integer(minVal, maxVal, minIn, maxIn);
+  generate_random_float(minVal, maxVal, minIn, maxIn);
   return 0; // signals the operating system that our program ended OK.
 }
 
+void generate_random_integer(int minVal, int maxVal, bool minIn, bool maxIn) {
+  int i = 0;
+  if (minIn == true && maxIn == true) {
+      for (i = 0; i < 10; i++) {
+      int randInt = rand() % (int)(maxVal - minVal + 1) + minVal;
+      cout << randInt << endl;
+    }
+  } else if (minIn == true && maxIn == false) {
+      for (i = 0; i < 10; i++) {
+        int randInt = rand() % (int)(maxVal - minVal + 1) + minVal;
+        if (randInt == maxVal){
+          randInt -= 1;
+        }
+        cout << randInt << endl;
+    }
+  } else if (minIn == false && maxIn == true) {
+    for (i = 0; i < 10; i++) {
+      int randInt = rand() % (int)(maxVal - minVal + 1) + minVal;
+      if (randInt == minVal){
+        randInt += 1;
+      }
+      cout << randInt << endl;
+    }
+  } else{
+    for (i = 0; i < 10; i++) {
+      int randInt = rand() % (int)(maxVal - minVal + 1) + minVal;
+      if (randInt == minVal) {
+        randInt += 1;
+      }else if(randInt == maxVal){
+        randInt -=1;
+      }
+      cout << randInt << endl;
+    }
+  }
+}
+void generate_random_float(float minVal, float maxVal, bool minIn, bool maxIn) {
+  int i = 0;
+  if (minIn == true && maxIn == true) {
+    for (i =0; i < 10; i++) {
+      float randFloat = rand() / (float)RAND_MAX * (maxVal - minVal) + minVal;
+      cout << setprecision(4) << randFloat << endl;
+    }
+  } else if (minIn == true && maxIn == false) {
+    for (i =0; i < 10; i++) {
+      float randFloat = rand() / (float)RAND_MAX * (maxVal - minVal) + minVal;
+      if(randFloat == maxVal){
+        randFloat -= .001;   
+      }
+      cout << setprecision(4) << randFloat << endl;
+    }
+  } else if (minIn == false && maxIn == true) {
+    for (i =0; i < 10; i++) {
+      float randFloat = rand() / (float)RAND_MAX * (maxVal - minVal) + minVal;
+      if(randFloat == maxVal){
+        randFloat += .001;   
+      }
+      cout << setprecision(4) << randFloat << endl;
+    }
+  } else{
+    for (i =0; i < 10; i++) {
+      float randFloat = rand() / (float)RAND_MAX * (maxVal - minVal) + minVal;
+      if(randFloat == maxVal){
+        randFloat -= .001;   
+      }
+      else if(randFloat == minVal){
+        randFloat += .001;
+      }
+      cout << setprecision(4) << randFloat << endl;
+    }
+  }
+}
