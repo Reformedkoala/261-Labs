cb307300db7998b7a4ebb102ebbc47b3a143a5ea
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-02-09 10:28:47 -0700

    Add files via upload

---
+++ Set2/L2E/calculator_functions.h
@@ -0,0 +1,7 @@
+void turn_calculator_on();
+void print_result(int a, int b, int result, char op);
+void print_options();
+void add(int a, int b);
+void sub(int a, int b);
+void mult(int a, int b);
+void divide(int a, int b);
