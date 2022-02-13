a20ae9463225e0d3c1003bc53fdcf583216e887f
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-02-13 16:57:00 -0700

    L2C

---
+++ Set2/L2C/main.cpp
@@ -0,0 +1,58 @@
+/* CSCI 261 Lab 2C: Pass-by-Value & Pass-by-Reference 
+ *
+ * Author: Garrett Thompson
+ *
+ * 
+ */
+
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+#include <cmath> // for math
+#include <cstdlib> // for random
+#include <ctime> // for time
+#include <iomanip> // for formatting
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+void polar_to_cartesian(const double radius, const double angle, double& xCoord, double& yCoord);
+void cartesian_to_polar(const double xCoord, const double yCoord, double& radius, double& angle);
+
+int main() {
+    int userChoice = 0;
+    cout << "Would you like to convert from polar to cartesian (r, θ) -> (x, y) or cartesian to polar (x, y) -> (r, θ)" << endl;
+    cout << "1) Polar to Cartesian" << endl;
+    cout << "2) Cartesian to Polar" << endl;
+    cout << "Enter Here: ";
+    cin >> userChoice;
+    double radius;
+    double angle;
+    double xCoord;
+    double yCoord;
+    if (userChoice == 1) {
+      cout << "Please enter the polar coordinates for radius then the angle in degrees." << endl;
+      cout << "Enter radius: ";
+      cin >> radius;
+      cout << "Enter angle in degrees: ";
+      cin >> angle;
+      polar_to_cartesian(radius, angle, xCoord, yCoord);
+      cout << "The cartesian values for the polar coordinates you entered are: " << xCoord << " , " << yCoord << endl;
+    }
+    else {
+      cout << "Please enter the polar coordinates for radius then the angle in degrees." << endl;
+      cout << "Enter X coordinate: ";
+      cin >> xCoord;
+      cout << "Enter Y coordinate: ";
+      cin >> yCoord;
+      cartesian_to_polar(xCoord, yCoord, radius, angle);
+      cout << "The polar values for the cartesian coordinates you entered are: " << radius << " , " << angle << endl;
+
+    }
+}
+
+void polar_to_cartesian(const double radius, const double angle, double& xCoord, double& yCoord) {
+  xCoord = radius * cos((angle*3.14159)/180);
+  yCoord = radius * sin((angle*3.14159)/180);
+}
+void cartesian_to_polar(const double xCoord, const double yCoord, double& radius, double& angle) {
+  radius = sqrt((xCoord*xCoord) + (yCoord*yCoord));
+  angle = (atan((yCoord/xCoord)) * 180)/3.14159;
+}
