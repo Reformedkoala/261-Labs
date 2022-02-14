98ffd5db76db637470eb0c8d403cab18e16893dc
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-02-14 14:45:31 -0700

    Added A2

---
+++ Set2/A2/ppm_functions.cpp
@@ -0,0 +1,17 @@
+/* CSCI 261 A2: PPM Image Editor
+ *
+ * Author: Garrett Thompson
+ *
+ * 
+ */
+
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+#include <cmath> // for math
+#include <cstdlib> // for random
+#include <ctime> // for time
+#include <iomanip> // for formatting
+#include "ppm_functions.h"
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+
