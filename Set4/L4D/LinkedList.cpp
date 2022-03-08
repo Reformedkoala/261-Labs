2fa422335249a390849f5c5c822c9abb0a83fe9a
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-03-07 21:48:21 -0700

    L4D Started

---
+++ Set4/L4D/LinkedList.cpp
@@ -0,0 +1,8 @@
+#include "Node.h"
+#include "LinkedList.h"
+
+LinkedList::LinkedList(){
+    mpHead = nullptr;
+    mpTail = nullptr;
+    msize = 0;
+}
--- Set4/L4D/LinkedList.h
+++ Set4/L4D/LinkedList.h
@@ -1,12 +1,17 @@
 #include "Node.h"
 class LinkedList {
-public:
-    Node* pHead;
-    Node* pTail;
-    unsigned int size;
+private:
+    Node* mpHead;
+    Node* mpTail;
+    unsigned int msize;
     Node* makeNodeForValue(int value);
+public:
+    int Front();
+    int Back();
+    unsigned int mize();
     void pushFront(int value);
     void pushBack(int value);
     int popFront();
-
+    LinkedList();
+    ~LinkedList();
 }
