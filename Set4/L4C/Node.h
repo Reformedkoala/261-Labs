d07aabd90c4786c691a6390db24ff178e3e4aaaf
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-03-29 22:32:05 -0600

    Update Node.h

--- Set4/L4C/Node.h
+++ Set4/L4C/Node.h
@@ -1,6 +1,12 @@
 #ifndef NODE_H
 #define NODE_H
-
+/* CSCI 261: L4C: The Class Shell
+ *
+ * Author: Garrett Thompson
+ *
+ * Making a shell to later implement a class for our Linked List and Node
+ *
+ */
 /**
  * @brief A single element of a linked list
  * 
