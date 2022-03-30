af8983f94d84f7abd427450550f24e8bab2094b6
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-03-29 22:32:02 -0600

    Update LinkedList.h

--- Set4/L4C/LinkedList.h
+++ Set4/L4C/LinkedList.h
@@ -1,6 +1,13 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 #include "Node.h"
+/* CSCI 261: L4C: The Class Shell
+ *
+ * Author: Garrett Thompson
+ *
+ * Making a shell to later implement a class for our Linked List and Node
+ *
+ */
 class LinkedList {
 public:
     Node* pHead;
