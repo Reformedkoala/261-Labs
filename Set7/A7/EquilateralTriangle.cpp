bd6457d25130344c004c0dab13cd3aa6aa777e87
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2022-04-29 16:02:37 -0600

    A7

--- Set7/A7/EquilateralTriangle.cpp
+++ Set7/A7/EquilateralTriangle.cpp
@@ -7,7 +7,7 @@ bool EquilateralTriangle::setCoordinates(double x1, double y1, double x2, double
   int d1 = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
   int d2 = sqrt((x3-x2)*(x3-x2) + (y3-y2)*(y3-y2));
   int d3 = sqrt((x1-x3)*(x1-x3) + (y1-y3)*(y1-y3));
-  
+
   if(d1 > 0 && d2 > 0 && d3 > 0 && ( d1+d2 >= d3 || d2+d3 >= d1 || d1+d3 >= d2 ) && ((d1==d2) && (d2 == d3) && (d3 == d1))){
     verticeList.push_back(x1);
     verticeList.push_back(y1);
@@ -18,7 +18,6 @@ bool EquilateralTriangle::setCoordinates(double x1, double y1, double x2, double
     return true;
   }
   else{
-    cout << d1 << " " << d2 << " " << d3 << endl;
     return false;
   }
 }
